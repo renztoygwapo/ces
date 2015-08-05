7a9de9cfe7642ce9d434928bbd2bcd5ddfa847b6
Raven <fuentesraven21@gmail.com>
2015-08-05 10:53:57 +0800

    add frontpage links

--- application/views/admin/components/frontpage_head.php
+++ application/views/admin/components/frontpage_head.php
@@ -61,3 +61,77 @@ License: You must have a valid license purchased only from themeforest (the abov
   <link href="<?php echo site_url('/assets/frontend/layout/css/custom.css')?>" rel="stylesheet">
   <!-- Theme styles END -->
 </head>
+
+<!-- Body BEGIN -->
+<body class="corporate">
+    <!-- BEGIN TOP BAR -->
+    <div class="pre-header">
+        <div class="container">
+            <div class="row">
+                <!-- BEGIN TOP BAR LEFT PART -->
+                <div class="col-md-6 col-sm-6 additional-shop-info">
+                    <ul class="list-unstyled list-inline">
+                        <li><i class="fa fa-child"></i><span>Welcome Guest!</span></li>
+                       
+                    </ul>
+                </div>
+                <!-- END TOP BAR LEFT PART -->
+                <!-- BEGIN TOP BAR MENU -->
+                <div class="col-md-6 col-sm-6 additional-nav">
+                    <ul class="list-unstyled list-inline pull-right">
+                        <li><a href="<?php echo site_url('admin/user/login/')?>">Log In</a></li>
+                        <li><a href="<?php echo site_url('admin/user/register/')?>">Registration</a></li>
+                    </ul>
+                </div>
+                <!-- END TOP BAR MENU -->
+            </div>
+        </div>        
+    </div>
+    <!-- END TOP BAR -->
+    <!-- BEGIN HEADER -->
+    <div class="header">
+      <div class="container">
+        <a class="site-logo" href="#"><img src="../../assets/frontend/layout/img/logos/logo-corp-red.png" alt="Metronic FrontEnd"></a>
+
+        <a href="javascript:void(0);" class="mobi-toggler"><i class="fa fa-bars"></i></a>
+
+        <!-- BEGIN NAVIGATION -->
+        <div class="header-navigation pull-right font-transform-inherit">
+          <ul>
+            <li class="dropdown">
+              <a href="<?php echo site_url()?>">Home</a>
+            </li>
+            <li class="dropdown">
+              <a href="<?php echo site_url('events/')?>">Events</a>  
+            </li>
+            <li class="dropdown">
+              <a href="<?php echo site_url('forum/')?>">Forum</a>
+            </li>
+            <li class="dropdown">
+              <a href="<?php echo site_url('gallery/')?>">Gallery</a>
+            </li>
+            <li class="dropdown">
+              <a href="<?php echo site_url('about/')?>">About Us</a>   
+            </li>
+            <!-- BEGIN TOP SEARCH -->
+            <li class="menu-search">
+              <span class="sep"></span>
+              <i class="fa fa-search search-btn"></i>
+              <div class="search-box">
+                <form action="#">
+                  <div class="input-group">
+                    <input type="text" placeholder="Search" class="form-control">
+                    <span class="input-group-btn">
+                      <button class="btn btn-primary" type="submit">Search</button>
+                    </span>
+                  </div>
+                </form>
+              </div> 
+            </li>
+            <!-- END TOP SEARCH -->
+          </ul>
+        </div>
+        <!-- END NAVIGATION -->
+      </div>
+    </div>
+    <!-- Header END -->
