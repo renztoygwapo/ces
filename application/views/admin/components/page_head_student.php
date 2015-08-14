65579086a701727dd614ee9ff4bf005053182056
RavenFuentes <fuentesraven21@gmail.com>
2015-08-14 23:02:06 +0800

    test

--- application/views/admin/components/page_head_student.php
+++ application/views/admin/components/page_head_student.php
@@ -170,28 +170,12 @@ License: You must have a valid license purchased only from themeforest(the above
 				<!-- DOC: To remove the sidebar toggler from the sidebar you just need to completely remove the below "sidebar-toggler-wrapper" LI element -->
 				<li class="sidebar-toggler-wrapper">
 					<!-- BEGIN SIDEBAR TOGGLER BUTTON -->
-					<div class="sidebar-toggler push-left">
+					<div class="sidebar-toggler">
+						
 					</div>
 					<!-- END SIDEBAR TOGGLER BUTTON -->
 				</li>
 				<!-- DOC: To remove the search box from the sidebar you just need to completely remove the below "sidebar-search-wrapper" LI element -->
-				<li class="sidebar-search-wrapper">
-					<!-- BEGIN RESPONSIVE QUICK SEARCH FORM -->
-					<!-- DOC: Apply "sidebar-search-bordered" class the below search form to have bordered search box -->
-					<!-- DOC: Apply "sidebar-search-bordered sidebar-search-solid" class the below search form to have bordered & solid search box -->
-					<form class="sidebar-search " action="extra_search.html" method="POST">
-						<a href="javascript:;" class="remove">
-						<i class="icon-close"></i>
-						</a>
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Search...">
-							<span class="input-group-btn">
-							<a href="javascript:;" class="btn submit"><i class="icon-magnifier"></i></a>
-							</span>
-						</div>
-					</form>
-					<!-- END RESPONSIVE QUICK SEARCH FORM -->
-				</li>
 				<li>
 					<a href="<?php echo site_url('student/')?>" >
 					<i class="icon-home"></i>
@@ -199,6 +183,7 @@ License: You must have a valid license purchased only from themeforest(the above
 					<span class="selected"></span>
 				
 					</a>
+
 				</li>
 				<li>
 					<a href="<?php echo site_url('student/profile')?>" >
