d739d4992d3e92efc3124d058371caf98e86d974
RavenFuentes <fuentesraven21@gmail.com>
2015-09-10 22:36:55 +0800

    admin header logo fix link

--- application/views/admin/student/student_list.php
+++ application/views/admin/student/student_list.php
@@ -102,7 +102,7 @@
 				<ul class="page-breadcrumb">
 					<li>
 						<i class="fa fa-home"></i>
-						<a href="index.html">Home</a>
+						<a href="<?php echo site_url('admin/home_admin/')?>">Home</a>
 						<i class="fa fa-angle-right"></i>
 					</li>
 					<li>
@@ -121,13 +121,16 @@
 				<div class="portlet box blue-hoki" style="display: block;">
 						<div class="portlet-title" style="display: block;">
 							<div class="caption">
-								<i class="fa fa-globe"></i>Datatable with TableTools
+								<i class="fa fa-globe"></i>Student List
 							</div>
 							<div class="tools">
 							</div>
 						</div>
 						<div class="portlet-body">
-							<div id="sample_1_wrapper" class="dataTables_wrapper no-footer"><div class="row" style="display: block;"><div class="col-md-12"><div class="btn-group tabletools-dropdown-on-portlet" style="display: block;"><a class="btn btn-sm default DTTT_button_pdf" id="ToolTables_sample_1_0"><span>PDF</span><div style="position: absolute; left: 0px; top: 0px; width: 44px; height: 28px; z-index: 99;"><embed id="ZeroClipboard_TableToolsMovie_1" src="../../assets/global/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf" loop="false" menu="false" quality="best" bgcolor="#ffffff" width="44" height="28" name="ZeroClipboard_TableToolsMovie_1" align="middle" allowscriptaccess="always" allowfullscreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" flashvars="id=1&amp;width=44&amp;height=28" wmode="transparent"></div></a><a class="btn btn-sm default DTTT_button_csv" id="ToolTables_sample_1_1"><span>CSV</span><div style="position: absolute; left: 0px; top: 0px; width: 43px; height: 28px; z-index: 99;"><embed id="ZeroClipboard_TableToolsMovie_2" src="../../assets/global/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf" loop="false" menu="false" quality="best" bgcolor="#ffffff" width="43" height="28" name="ZeroClipboard_TableToolsMovie_2" align="middle" allowscriptaccess="always" allowfullscreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" flashvars="id=2&amp;width=43&amp;height=28" wmode="transparent"></div></a><a class="btn btn-sm default DTTT_button_xls" id="ToolTables_sample_1_2"><span>Excel</span><div style="position: absolute; left: 0px; top: 0px; width: 51px; height: 28px; z-index: 99;"><embed id="ZeroClipboard_TableToolsMovie_3" src="../../assets/global/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf" loop="false" menu="false" quality="best" bgcolor="#ffffff" width="51" height="28" name="ZeroClipboard_TableToolsMovie_3" align="middle" allowscriptaccess="always" allowfullscreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" flashvars="id=3&amp;width=51&amp;height=28" wmode="transparent"></div></a><a class="btn btn-sm default DTTT_button_print" id="ToolTables_sample_1_3" title="View print view"><span>Print</span></a></div></div></div><div class="row" style="display: block;"><div class="col-md-6 col-sm-12"><div class="dataTables_length" id="sample_1_length" style="display: block;"><label> <div class="select2-container form-control input-xsmall input-inline" id="s2id_autogen1"><a href="javascript:void(0)" class="select2-choice" tabindex="-1">   <span class="select2-chosen" id="select2-chosen-2">5</span><abbr class="select2-search-choice-close"></abbr>   <span class="select2-arrow" role="presentation"><b role="presentation"></b></span></a><label for="s2id_autogen2" class="select2-offscreen"></label><input class="select2-focusser select2-offscreen" type="text" aria-haspopup="true" role="button" aria-labelledby="select2-chosen-2" id="s2id_autogen2"><div class="select2-drop select2-display-none select2-with-searchbox">   <div class="select2-search">       <label for="s2id_autogen2_search" class="select2-offscreen"></label>       <input type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" class="select2-input" role="combobox" aria-expanded="true" aria-autocomplete="list" aria-owns="select2-results-2" id="s2id_autogen2_search" placeholder="">   </div>   <ul class="select2-results" role="listbox" id="select2-results-2">   </ul></div></div><select name="sample_1_length" aria-controls="sample_1" class="form-control input-xsmall input-inline select2-offscreen" tabindex="-1" title=""><option value="5">5</option><option value="15">15</option><option value="20">20</option><option value="-1">All</option></select> records </label></div></div><div class="col-md-6 col-sm-12"><div id="sample_1_filter" class="dataTables_filter" style="display: block;"><label>Search:<input type="search" class="form-control input-small input-inline" aria-controls="sample_1"></label></div></div></div><div class="table-scrollable"><table class="table table-striped table-bordered table-hover dataTable no-footer" id="sample_1" role="grid" aria-describedby="sample_1_info">
+							<div id="sample_1_wrapper" class="dataTables_wrapper no-footer">
+								
+							<div class="table-scrollable">
+							<table class="table table-striped table-bordered table-hover dataTable no-footer" id="sample_1" role="grid" aria-describedby="sample_1_info">
 							<thead>
 							<tr role="row">
 								<th class="sorting_asc" tabindex="0" aria-controls="sample_1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="
@@ -155,7 +158,7 @@
 							
 					
 								<td class="sorting_1">
-									<img src="<?php echo $row->photo;?>"> 
+									<img src="<?php echo $row->photo;?>" style="height: 28px;width: 36px;"> 
 								</td>
 								<td>
 									<?php echo $row->firstname;?>
